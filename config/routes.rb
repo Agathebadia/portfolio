66e5dd2f1f16d99be34b6f1f096018cbfdc259a8
Agathe Badia <ab@honeypot.io>
2019-12-13 12:06:41 +0100

    added home page, routes and controller method

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/contact'
-  get 'pages/about'
+  root to: 'pages#home'
+  get 'about', to: 'pages#about', as: :about
+  get 'contact', to: 'pages#contact', as: :contact
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
